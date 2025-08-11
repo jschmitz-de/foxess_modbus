a70e5c47411d8f0d8bf937ac79806878276f9beb
jschmitz-de <38846096+jschmitz-de@users.noreply.github.com>
2025-08-11 16:23:30 +0200

    Update entity_descriptions.py
    Add _bms_1_master_version for Inv.H3_PRO_122

--- custom_components/foxess_modbus/entities/entity_descriptions.py
+++ custom_components/foxess_modbus/entities/entity_descriptions.py
@@ -75,6 +75,22 @@ def _version_entities() -> Iterable[EntityFactory]:
         is_hex=True,
     )
 
+        
+  def _bms_1_master_version(address: list[ModbusAddressSpec], is_hex: bool) -> ModbusVersionSensorDescription:
+    return ModbusVersionSensorDescription(
+      key="bms_1_master_version",
+      address=address,
+      is_hex=is_hex,
+      name="Version: BMS 1 Master",
+      icon="mdi:source-branch",
+    )
+
+    yield _bms_1_master_version(
+        address=[
+            ModbusAddressSpec(holding=37003, models=Inv.H3_PRO_122),
+        ],
+        is_hex=True,
+
     def _slave_version(address: list[ModbusAddressSpec], is_hex: bool) -> ModbusVersionSensorDescription:
         return ModbusVersionSensorDescription(
             key="slave_version",
